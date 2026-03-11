3b3d267893a3ed9fd5245bdf3e1db6b0aa45c6a2
2022M136 <2022m136@hokuriku-u.ac.jp>
2026-03-11 19:58:31 +0900

    Updates

---
+++ _next/static/NopjJnJAAT0TYnp2sdzzo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-467872e7ac2f1f7f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
